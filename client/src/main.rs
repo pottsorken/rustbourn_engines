1aba5bc71fb6dba88eebc83cbf498766c65f66c9
pottsorken <163292800+pottsorken@users.noreply.github.com>
2025-03-28 13:46:04 +0100

    Customize game window

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,6 +2,30 @@ use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins
+            .set(WindowPlugin {
+                primary_window : Some(Window{
+                    title : String::from("Rustbourn Engines"),
+                    position : WindowPosition::Centered(MonitorSelection::Primary),
+                    ..Default::default()
+                }),
+                ..Default::default()
+            })
+        )
+        .add_systems(Startup, setup_map)
         .run();
+}
+
+#[derive(Component)]
+
+struct Base{
+    pub velocity : f32,
+}
+
+fn setup_map(
+    mut commands : Commands
+){
+    commands.insert_resource(ClearColor(Color::srgb(0.37, 0.5, 0.0))); // rgb(37.6% 50.7% 0%)
+
+    commands.spawn(Camera2d::default());
 }
