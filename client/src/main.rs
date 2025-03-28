cf3761616d2f5fcc60460f3bf6bbdf2a87bb70d7
TG <tbjorkdahl@hotmail.com>
2025-03-28 16:25:02 +0100

    movement, rotation and object with no collision

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,54 +1,105 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Player;
-
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, player_movement)
+        .add_systems(Update, gather_input) // runs on client
+        .add_systems(FixedUpdate, apply_movement) // runs on server
         .run();
 }
 
+#[derive(Component)]
+struct Player {
+    movement_speed: f32,
+    rotation_speed: f32,
+}
+
+#[derive(Component)]
+struct Obstacle {
+
+}
+
+#[derive(Component, Default)]
+struct MoveIntent {
+    rotation: f32,
+    forward: f32,
+}
+
 fn setup(mut commands: Commands) {
-    // Spawn camera
     commands.spawn(Camera2dBundle::default());
 
-    // Spawn a player sprite at position (0, 0)
     commands.spawn((
         SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.3, 0.7, 0.9),
+                custom_size: Some(Vec2::new(50.0, 100.0)),
+                ..default()
+            },
             transform: Transform::from_xyz(0.0, 0.0, 0.0),
-            ..Default::default()
+            ..default()
         },
-        Player,
+        Player {
+            movement_speed: 500.0,
+            rotation_speed: f32::to_radians(180.0),
+        },
+        MoveIntent::default(),
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(1.0, 0.0, 0.0),
+                custom_size: Some(Vec2::new(50.0, 50.0)),
+                ..default()
+            },
+            transform: Transform::from_xyz(100.0, 100.0, 0.0),
+            ..default()
+        },
+        Obstacle {
+        },
+        MoveIntent::default(),
     ));
 }
 
-fn player_movement(
+
+/// Client: Read input and store desired movement direction
+fn gather_input(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Transform, With<Player>>,
-    time: Res<Time>,
+    mut query: Query<&mut MoveIntent, With<Player>>,
 ) {
-    let mut direction = Vec3::ZERO;
+    for mut intent in &mut query {
+        intent.rotation = 0.0;
+        intent.forward = 0.0;
 
-    if keyboard_input.pressed(KeyCode::KeyW) {
-        direction.y += 1.0;
+        if keyboard_input.pressed(KeyCode::KeyA) {
+            intent.rotation += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyD) {
+            intent.rotation -= 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyW) {
+            intent.forward += 1.0;
+        }
+        if keyboard_input.pressed(KeyCode::KeyS) {
+            intent.forward -= 1.0;
+        }
     }
-    if keyboard_input.pressed(KeyCode::KeyS) {
-        direction.y -= 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::KeyA) {
-        direction.x -= 1.0;
-    }
-    if keyboard_input.pressed(KeyCode::KeyD) {
-        direction.x += 1.0;
-    }
+}
 
-    if direction != Vec3::ZERO {
-        direction = direction.normalize();
-        for mut transform in &mut query {
-            transform.translation += direction * 100.0 * time.delta_secs();
+/// Server: Apply movement based on client intent
+fn apply_movement(
+    time: Res<Time>,
+    mut query: Query<(&mut Transform, &Player, &MoveIntent)>,
+) {
+    for (mut transform, player, intent) in &mut query {
+        if intent.rotation != 0.0 {
+            transform.rotate_z(intent.rotation * player.rotation_speed * time.delta_secs());
+        }
+
+        if intent.forward != 0.0 {
+            let forward = transform.rotation * Vec3::Y;
+            transform.translation += forward * intent.forward * player.movement_speed * time.delta_secs();
         }
     }
 }
