13301058d513bb1b13573b8fcebd220c28f3eac7
pottsorken <163292800+pottsorken@users.noreply.github.com>
2025-04-02 09:27:13 +0200

    Moved camera functions

---
+++ client/src/camera.rs
@@ -0,0 +1,34 @@
+use bevy::prelude::*;
+
+use crate::player::{Player,
+    setup_player,
+    player_movement};
+
+pub fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 999.9),
+        ..default()
+    });
+}
+
+
+pub fn camera_follow(
+    player_query: Query<&Transform, With<Player>>,
+    mut camera_query: Query<&mut Transform, (With<Camera>, Without<Player>)>,
+    time: Res<Time>,
+) {
+    let follow_speed = 3.0;
+
+    if let Ok(player_transform) = player_query.get_single() {
+        for mut camera_transform in camera_query.iter_mut() {
+            camera_transform.translation = camera_transform.translation.lerp(
+                Vec3::new(
+                    player_transform.translation.x,
+                    player_transform.translation.y,
+                    camera_transform.translation.z, // Ensure camera z-pos unchanged
+                ),
+                follow_speed * time.delta_secs(),
+            );
+        }
+    }
+}
