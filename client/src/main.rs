c60e33471310f94b0eeb2d6aa6fca153ef4d4ad0
Morgan Bjälvenäs <morganbj@kth.se>
2025-04-02 08:30:57 +0200

    add test client

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,31 +2,26 @@ use bevy::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
 use noisy_bevy::simplex_noise_2d; // For map generation. May be temporary
 
-mod player;
-mod map;
 mod camera;
+mod map;
+mod player;
 
-use player::{Player,
-            setup_player,
-            player_movement};
+use player::{player_movement, setup_player, Player};
 
-use map::{setup_tilemap};
+use map::setup_tilemap;
 
-use camera::{setup_camera,
-            camera_follow};
+use camera::{camera_follow, setup_camera};
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins
-            .set(WindowPlugin {
-                primary_window : Some(Window{
-                    title : String::from("Rustbourn Engines"),
-                    position : WindowPosition::Centered(MonitorSelection::Primary),
-                    ..Default::default()
-                }),
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: String::from("Rustbourn Engines"),
+                position: WindowPosition::Centered(MonitorSelection::Primary),
                 ..Default::default()
-            })
-        )
+            }),
+            ..Default::default()
+        }))
         .add_plugins(TilemapPlugin)
         .add_systems(Startup, (setup_camera, setup_player, setup_tilemap))
         .add_systems(Update, (player_movement, camera_follow))
