b3a7eb842a7c64ec1c3d9c3c097e6e63dd522d82
pottsorken <163292800+pottsorken@users.noreply.github.com>
2025-04-02 08:40:54 +0200

    Added some minor styling

---
+++ client/src/map.rs
@@ -0,0 +1,11 @@
+// use bevy::preluse::*;
+
+// pub struct MapPlugin;
+
+// impl Plugin for MapPlugin{
+//     fn build(&self, app: &mut App) {
+//         app.add_startup_system(create_map)
+//     }
+// }
+
+// fn create_map(mut )
