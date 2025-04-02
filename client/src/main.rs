07bca7bb7f63e022e981267a7a98701db9c392c6
pottsorken <163292800+pottsorken@users.noreply.github.com>
2025-04-02 09:04:35 +0200

    Moved Player struct and setup function to separate file

--- client/src/main.rs
+++ client/src/main.rs
@@ -2,13 +2,11 @@ use bevy::prelude::*;
 use bevy_ecs_tilemap::prelude::*;
 use noisy_bevy::simplex_noise_2d; // For map generation. May be temporary
 
-#[derive(Component)]
-struct Player {
-    /// linear speed in meters per second
-    movement_speed: f32,
-    /// rotation speed in radians per second
-    rotation_speed: f32,
-}
+mod player;
+
+use player::Player;
+
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins
@@ -22,7 +20,7 @@ fn main() {
             })
         )
         .add_plugins(TilemapPlugin)
-        .add_systems(Startup, (setup_camera, setup_player, setup_tilemap))
+        .add_systems(Startup, (setup_camera, player::setup_player, setup_tilemap))
         .add_systems(Update, (player_movement, camera_follow))
         .run();
 }
@@ -34,25 +32,7 @@ fn setup_camera(mut commands: Commands) {
     });
 }
 
-fn setup_player(mut commands: Commands, asset_server: Res<AssetServer>) {
-    // Spawn a player sprite at position (0, 0) at a higher z-index than map
-    commands.spawn((
-        Sprite {
-            custom_size: Some(Vec2::new(80.0, 80.0)), // Square size 100x100 pixels
-            image: asset_server.load("sprites/top-view/robot_3Dblue.png"),
-            ..default()
-        },
-        //TextureAtlas {
-        //    layout: asset_server.load("sprites/top-view/robot_3Dblue.png"),
-        //    index: 0,
-        //}, -- NOTE: If asset-chart is ever used
-        Transform::from_xyz(0.0, 0.0, 1.0),
-        Player {
-            movement_speed: 300.0,                  // meters per second
-            rotation_speed: f32::to_radians(180.0), // degrees per second
-        },
-    ));
-}
+// player::setup_player(Commands, Res<AssetServer>);
 
 fn setup_tilemap(mut commands: Commands, asset_server: Res<AssetServer>) {
     let texture_handle: Vec<Handle<Image>> = vec![
