06924a6ec0b5d85e1a335df166ef0c49f1253c9e
TG <tbjorkdahl@hotmail.com>
2025-04-03 15:34:10 +0200

    I have given up

--- client/src/main_menu/systems/layout.rs
+++ client/src/main_menu/systems/layout.rs
@@ -1,7 +1,11 @@
-use bevy::prelude::*;
-use bevy::ui::*;
-use bevy::text::*;
+use bevy::{
+    prelude::*,
+    text::*,
+    color::palettes::basic::*,
+    winit::WinitSettings,
+};
 
+use crate::Val::*;
 use crate::main_menu::components::*;
 use crate::main_menu::styles::*;
 
@@ -13,47 +17,59 @@ pub fn spawn_main_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
 }
 
 pub fn build_main_menu(commands: &mut Commands, asset_server: &Res<AssetServer>) -> Entity {
-    // Setup button style
-    let button_style = Node {
-        width: Val::Px(200.0),
-        height: Val::Px(80.0),
-        margin: UiRect::all(Val::Px(10.0)),
-        justify_content: JustifyContent::Center,
-        align_items: AlignItems::Center,
-        ..default()
-    };
 
-    // Setup button text style
-    let button_text_style = TextFont {
-        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
-        font_size: 32.0,
-        font_smoothing: FontSmoothing::None,
-    };
-
+    // Root UI node with horizontal layout
     commands
         .spawn((
-            Node::default(),
+            Node {
+                right: Val::Percent(-90.),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                flex_direction: FlexDirection::Row,
+                ..default()
+            },
             MainMenuRoot,
         ))
-        .insert(Node {
-            justify_content: JustifyContent::Center,
-            align_items: AlignItems::Center,
-            ..default()
-        })
-        .insert(BackgroundColor(Color::srgb(0.1, 0.1, 0.1)))
         .with_children(|parent| {
+            // Left-side vertical button panel
             parent
-                .spawn((
-                    Button::default(),
-                    PlayButton,
-                ))
-                .insert(button_style.clone())
-                .insert(button_text_style.clone())
-                .insert(BackgroundColor(NORMAL_BUTTON.into()))
-                .with_children(|parent| {
-                    parent.spawn(Text::new(
-                        "Block",
-                    ));
+                .spawn(Node {
+                        flex_direction: FlexDirection::Column,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::Center,
+                        padding: UiRect::all(Val::Px(20.0)),
+                        ..default()
+                })
+                .with_children(|panel| {
+                    for label in &["Block 1", "Block 2", "Block 3", "Block 4"] {
+                        panel
+                            .spawn((
+                                Button,
+                                Node {
+                                    width: Val::Px(50.0),
+                                    height: Val::Px(50.0),
+                                    margin: UiRect::all(Val::Px(10.0)),
+                                    justify_content: JustifyContent::Center,
+                                    align_items: AlignItems::Center,
+                                    ..default()
+                                },
+                                BorderColor(Color::BLACK),
+                                BackgroundColor(NORMAL_BUTTON),
+                                PlayButton, // You can change this per label if needed
+                            ))
+                            .with_children(|button| {
+                                button.spawn((
+                                    Text::new(*label),
+                                    TextFont {
+                                        font_size: 10.0,
+                                        ..Default::default()
+                                    },
+                                    TextColor {
+                                        0: Color::WHITE,
+                                    },
+                                ));
+                            });
+                    }
                 });
         })
         .id()
