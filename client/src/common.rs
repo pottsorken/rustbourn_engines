73889934b6fd7b4cad735c0997026e70498f9624
pottsorken <163292800+pottsorken@users.noreply.github.com>
2025-05-08 16:53:08 +0200

    tiiiiiiles

--- client/src/common.rs
+++ client/src/common.rs
@@ -250,7 +250,7 @@ pub struct MapConfig {
     pub map_size: TilemapSize,
     pub tile_size: TilemapTileSize,
     pub noise_scale: f32, // Grid size == tile size
-    pub tile_textures: [&'static str; 13], // Change this for the number of tiles in the list
+    pub tile_textures: [&'static str; 18], // Change this for the number of tiles in the list
     pub image_path: &'static str,
     pub safe_zone_size: f32,
 }
