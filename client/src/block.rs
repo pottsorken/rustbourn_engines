de69bf78831c4ec5e709ba4b278b972fdb6c699a
pottsorken <163292800+pottsorken@users.noreply.github.com>
2025-05-09 09:59:17 +0200

    Tried with few things spawning

--- client/src/block.rs
+++ client/src/block.rs
@@ -22,32 +22,31 @@ pub fn setup_block(
         entities: HashMap::new(),
     });
 
-    //let _window = window_query.get_single().unwrap();
-    //
-    //let world_map_size = Vec2::new(
-    //    MAP_CONFIG.map_size.x as f32 * MAP_CONFIG.tile_size.x / 2.0,
-    //    MAP_CONFIG.map_size.y as f32 * MAP_CONFIG.tile_size.y / 2.0,
-    //);
-    //
-    //for _ in 0..BLOCK_CONFIG.count {
-    //    let random_x = (random::<f32>() - 0.5) * world_map_size.x;
-    //    let random_y = (random::<f32>() - 0.5) * world_map_size.y;
-    //    let valid_x = random_x < MAP_CONFIG.safe_zone_size && random_x > -MAP_CONFIG.safe_zone_size;
-    //    let valid_y = random_y < MAP_CONFIG.safe_zone_size && random_y > -MAP_CONFIG.safe_zone_size;
-    //    if valid_x && valid_y {
-    //        continue;
-    //    }
-    //
-    //    commands.spawn((
-    //        Sprite {
-    //            custom_size: Some(BLOCK_CONFIG.size),
-    //            image: asset_server.load(BLOCK_CONFIG.path),
-    //            ..default()
-    //        },
-    //        Transform::from_xyz(random_x, random_y, 1.0),
-    //        Block {},
-    //    ));
-    //}
+    let world_map_size = Vec2::new(
+        MAP_CONFIG.map_size.x as f32 * MAP_CONFIG.tile_size.x / 2.0,
+        MAP_CONFIG.map_size.y as f32 * MAP_CONFIG.tile_size.y / 2.0,
+    );
+
+    for _ in 0..BLOCK_CONFIG.count {
+        let random_x = (random::<f32>() - 0.5) * world_map_size.x;
+        let random_y = (random::<f32>() - 0.5) * world_map_size.y;
+        let valid_x = random_x < MAP_CONFIG.safe_zone_size && random_x > -MAP_CONFIG.safe_zone_size;
+        let valid_y = random_y < MAP_CONFIG.safe_zone_size && random_y > -MAP_CONFIG.safe_zone_size;
+        if valid_x && valid_y {
+            continue;
+        }
+        let num = rand::thread_rng().gen_range(0..=3);
+
+        // commands.spawn((
+        //     Sprite {
+        //         custom_size: Some(BLOCK_CONFIG.size),
+        //         image: asset_server.load(BLOCK_CONFIG.path[num]),
+        //         ..default()
+        //     },
+        //     Transform::from_xyz(random_x, random_y, 1.0),
+        //     Block {},
+        // ));
+    }
 }
 
 pub fn update_block(
