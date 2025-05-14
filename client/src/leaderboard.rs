86934310f388fa8ee1f33085e72fd9c99571d8b3
Rimon Islam <rimon.islm03@gmail.com>
2025-05-14 09:12:04 +0200

    Added text to the leaderboard, that resizes with leaderboard

--- client/src/leaderboard.rs
+++ client/src/leaderboard.rs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 use crate::common::{
     CtxWrapper, Leaderboard, LeaderboardEntry, OnMainMenuScreen, Player, PlayerAttach,
     LEADRERBOARD_CONFIG,
@@ -5,6 +6,15 @@ use crate::common::{
 use crate::module_bindings::PlayerTableAccess;
 use crate::start_menu::*;
 use bevy::prelude::*;
+=======
+use bevy::{
+    color::palettes::css::*,
+    math::ops,
+    prelude::*,
+    sprite::Anchor,
+    text::{FontSmoothing, LineBreak, TextBounds},
+};
+>>>>>>> 3903aaf (Added text to the leaderboard, that resizes with leaderboard)
 use bevy::render::view::window;
 use clap::builder::styling::Style;
 
@@ -12,6 +22,12 @@ const SCOREBOARD_FONT_SIZE: f32 = 40.0;
 const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
 const TEXT_COLOR: Color = Color::srgba(0.5, 0.5, 1.0, 0.0);
 const SCORE_COLOR: Color = Color::srgba(1.0, 0.5, 0.5, 1.0);
+
+
+#[derive(Component)]
+struct AnimateScale;
+
+
 
 pub fn spawn_leaderboard(
     mut commands: Commands,
@@ -21,6 +37,13 @@ pub fn spawn_leaderboard(
     player_query: Query<&Transform, With<Player>>,
 ) {
     if query.is_empty() {
+
+        // Demonstrate text wrapping
+        let slightly_smaller_text_font = TextFont {
+            font_size: 35.0,
+            ..default()
+        };
+
         let leaderboard_size = Vec2::new(200.0, 300.0);
         let leaderboard_offset = Vec2::new(-800.0, 300.0);
 
@@ -42,7 +65,24 @@ pub fn spawn_leaderboard(
                     scale: Vec3::ONE,
                     ..default()
                 },
-            ));
+            )).with_children(|builder|{
+                builder.spawn((
+                    Text2d::new("jarvis the superior overlord"),
+                    TextFont {
+                        font_size: 15.0,
+                        ..default()
+                    },
+                    TextColor::BLACK,
+                    TextLayout::new(JustifyText::Left, LineBreak::WordBoundary),
+                    TextBounds::from(leaderboard_size),
+                    Transform::from_translation(Vec3::Z),
+
+
+                ));
+            }) 
+            
+            ;
+            
         }
     }
 }
